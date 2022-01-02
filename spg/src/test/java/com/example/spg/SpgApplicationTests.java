b2515f29e9fede93db70cd814f8242c537420164
yongbomb90 <yongBomb90@gmail.com>
2022-01-02 16:00:28 +0900

    init project

---
+++ spg/src/test/java/com/example/spg/SpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
