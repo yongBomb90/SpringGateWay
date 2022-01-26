396999c5f68fb3e6b11a85961b0b4dda2cc81c56
yongbomb90 <yongBomb90@gmail.com>
2022-01-26 22:51:00 +0900

    feclient

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,16 +1,20 @@
 package com.example.userservice;
 
+import com.example.userservice.error.FeignErrorDecoder;
+import feign.Logger;
+import jdk.jfr.Enabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class UserServiceApplication {
 
     public static void main(String[] args) {
@@ -27,4 +31,16 @@ public class UserServiceApplication {
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
+
+    @Bean
+    public Logger.Level feignLoggerLever() {
+        return Logger.Level.FULL;
+    }
+
+//    @Bean
+//    public FeignErrorDecoder feignErrorDecoder() {
+//        return new FeignErrorDecoder();
+//    }
+
+
 }
--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package com.example.userservice.service;
 
+import com.example.userservice.client.OrderServiceclient;
 import com.example.userservice.dto.UserDTO;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.modelmapper.spi.MatchingStrategy;
@@ -25,6 +28,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService{
 
 
@@ -37,12 +41,15 @@ public class UserServiceImpl implements UserService{
 
     Environment environment;
 
+    OrderServiceclient orderServiceclient;
+
     @Autowired
-    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder, RestTemplate restTemplate, Environment environment) {
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder, RestTemplate restTemplate, Environment environment,  OrderServiceclient orderServiceclient) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.restTemplate = restTemplate;
         this.environment = environment;
+        this.orderServiceclient = orderServiceclient;
     }
 
     @Override
@@ -78,17 +85,26 @@ public class UserServiceImpl implements UserService{
         UserDTO userDTO = new ModelMapper().map(userEntity,UserDTO.class);
 
 //        List<ResponseOrder> orders = new ArrayList<>();
-        String orderUrl = String.format(environment.getProperty("order_service.url"),userId) ;
+//        String orderUrl = String.format(environment.getProperty("order_service.url"),userId) ;
+//
+//        ResponseEntity<List<ResponseOrder>> orderListResponse =
+//        restTemplate.exchange(
+//                orderUrl
+//                , HttpMethod.GET
+//                , null
+//                , new ParameterizedTypeReference<List<ResponseOrder>>() {
+//                }
+//        );
+//        List<ResponseOrder> orders  = orderListResponse.getBody();
+        List<ResponseOrder> orders = null;
+//        try {
+//            orders = orderServiceclient.getOrders(userId);
+//        }catch (FeignException e) {
+//            log.error(e.toString());
+//        }
 
-        ResponseEntity<List<ResponseOrder>> orderListResponse =
-        restTemplate.exchange(
-                orderUrl
-                , HttpMethod.GET
-                , null
-                , new ParameterizedTypeReference<List<ResponseOrder>>() {
-                }
-        );
-        List<ResponseOrder> orders  = orderListResponse.getBody();
+        orders = orderServiceclient.getOrders(userId);
+
         userDTO.setOrders(orders);
         return userDTO;
     }
